b6105acae683f17f58423603fee0f9f492205ff8
Caedin Cook <caco5646@colorado.edu>
2023-12-04 11:56:22 +0100

    changed nav to its own file and updated script

---
+++ script/index.js
@@ -0,0 +1,56 @@
+document.addEventListener("DOMContentLoaded", function() {
+    document.getElementById("addressForm").addEventListener("submit", handleAddress);
+});
+
+let autocomplete
+function initMap() {
+ autocomplete = new google.maps.places.Autocomplete(
+    document.getElementById('domicilio_address'), 
+    {
+        types: ['address'],
+        componentRestrictions: { country: ['ES'] },
+        fields: ['address_components', 'geometry']
+    });
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        console.log("Geolocation is not supported by this browser.");
+    }
+}
+
+function showPosition(position) {
+    //convert lat and lng to address
+    var geocoder = new google.maps.Geocoder();
+    var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
+    geocoder.geocode(
+        { 'latLng': latlng }, 
+        function (results, status) {
+            if (status == google.maps.GeocoderStatus.OK) {
+                if (results[0]) {
+                    document.getElementById("domicilio_address").value = results[0].formatted_address;
+                }
+            }
+        }
+    );
+}
+/**
+ * 
+ * @param {SubmitEvent} e 
+ */
+function handleAddress(e) {
+    
+    e.preventDefault();
+    var address = document.getElementById("domicilio_address").value;
+    var data = {
+        address: address,
+        //find which tab is active to determine a domicilio or a recogida
+        type: document.getElementById("tab_container").getElementsByClassName("nav-link active")[0].id == "nav-domicilio-tab" ? "domicilio" : "recogida"
+    }
+    window.sessionStorage.setItem("addressDetails", JSON.stringify(data));
+    //redirect to menu page
+    window.location.href = "./carta.html";
+    console.log(address);
+}
--- script/script.js
+++ script/script.js
@@ -1,25 +1,34 @@
-document.addEventListener("DOMContentLoaded", function() {
-    document.getElementById("addressForm").addEventListener("submit", handleAddress);
+//an event listener that runs before the page is loaded
+// document.addEventListener("DOMContentLoaded", function() {
+fetch('/nav.html').then(function (response) {
+    return response.text();
+}).then(function (data) {
+    document.getElementById('nav-head').innerHTML = data;
 });
+fetch('/footer.html').then(function (response) {
+    return response.text();
+}).then(function (data) {
+    document.getElementById('footer').innerHTML = data;
+});
+document.addEventListener("DOMContentLoaded", async function () {
 
-document.addEventListener("DOMContentLoaded", function () {
     const userEmail = Cookies.get('userEmail');
     const isLoggedIn = Cookies.get('isLoggedIn')
 
     if (isLoggedIn) {
         // El usuario está autenticado, cambia el contenido del contenedor de acciones de usuario
         const userActionsContainer = document.querySelector('.user-actions');
-    
+
         // Iniciar sesión (oculto)
         const loginLink = document.getElementById('loginLink');
         loginLink.style.display = 'none';
-    
+
         // Cerrar sesión (visible)
         const dropdown = userActionsContainer.querySelector('.dropdown');
         const userEmailSpan = document.getElementById('userEmail');
         userEmailSpan.innerText = userEmail;  // Actualiza el contenido con el correo electrónico
         dropdown.style.display = 'inline';
-    
+
         logoutLink.addEventListener('click', function () {
             // Elimina las cookies al cerrar sesión
             Cookies.remove('userEmail');
@@ -32,56 +41,3 @@ document.addEventListener("DOMContentLoaded", function () {
 
     // Resto del código general
 });
-
-let autocomplete
-function initMap() {
- autocomplete = new google.maps.places.Autocomplete(
-    document.getElementById('domicilio_address'), 
-    {
-        types: ['address'],
-        componentRestrictions: { country: ['ES'] },
-        fields: ['address_components', 'geometry']
-    });
-}
-
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition);
-    } else {
-        console.log("Geolocation is not supported by this browser.");
-    }
-}
-
-function showPosition(position) {
-    //convert lat and lng to address
-    var geocoder = new google.maps.Geocoder();
-    var latlng = new google.maps.LatLng(position.coords.latitude, position.coords.longitude);
-    geocoder.geocode(
-        { 'latLng': latlng }, 
-        function (results, status) {
-            if (status == google.maps.GeocoderStatus.OK) {
-                if (results[0]) {
-                    document.getElementById("domicilio_address").value = results[0].formatted_address;
-                }
-            }
-        }
-    );
-}
-/**
- * 
- * @param {SubmitEvent} e 
- */
-function handleAddress(e) {
-    
-    e.preventDefault();
-    var address = document.getElementById("domicilio_address").value;
-    var data = {
-        address: address,
-        //find which tab is active to determine a domicilio or a recogida
-        type: document.getElementById("tab_container").getElementsByClassName("nav-link active")[0].id == "nav-domicilio-tab" ? "domicilio" : "recogida"
-    }
-    window.sessionStorage.setItem("addressDetails", JSON.stringify(data));
-    //redirect to menu page
-    window.location.href = "./carta.html";
-    console.log(address);
-}
