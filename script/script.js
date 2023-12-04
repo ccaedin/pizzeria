1e42ab357c87a69c1eb7be6f2e4814bf81ebc0ac
Caedin Cook <caco5646@colorado.edu>
2023-12-05 00:16:22 +0100

    fixed linkes

--- script/script.js
+++ script/script.js
@@ -4,13 +4,25 @@ fetch('/nav.html').then(function (response) {
     return response.text();
 }).then(function (data) {
     document.getElementById('nav-head').innerHTML = data;
+    //set athe active class on the current page
+    var url = window.location.pathname;
+    var filename = url.substring(url.lastIndexOf('/') + 1);
+    var navItems = document.getElementsByClassName('nav-item');
+    for (var i = 0; i < navItems.length; i++) {
+        var href = navItems[i].getElementsByTagName('a')[0].getAttribute('href');
+        if (filename == href) {
+            navItems[i].classList.add('active');
+        }
+    }
 });
 fetch('/footer.html').then(function (response) {
     return response.text();
 }).then(function (data) {
-    document.getElementById('footer').innerHTML = data;
+    var footer =document.getElementById('footer')
+    footer.innerHTML = data;
+    footer.classList.add('mt-auto');
 });
-document.addEventListener("DOMContentLoaded", async function () {
+document.addEventListener("DOMContentLoaded", function () {
 
     const userEmail = Cookies.get('userEmail');
     const isLoggedIn = Cookies.get('isLoggedIn')
