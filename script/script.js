6bd771fd1742ae49518c8bfb03979be3bf4c2993
Caedin Cook <caco5646@colorado.edu>
2023-12-11 11:28:18 +0100

    fixed cookie script from not loading

--- script/script.js
+++ script/script.js
@@ -3,6 +3,8 @@
 fetch('/nav.html').then(function (response) {
     return response.text();
 }).then(function (data) {
+    
+
     document.getElementById('nav-head').innerHTML = data;
     //set athe active class on the current page
     var url = window.location.pathname;
@@ -19,45 +21,54 @@ fetch('/nav.html').then(function (response) {
             }
         }
     }
-    if (localStorage.getItem("cart") != null) {
-        var cart = JSON.parse(localStorage.getItem("cart"));
-        var cartCount = 0;
-        for (var product in cart) {
-            cartCount += cart[product];
+    var s = document.createElement("script");
+    s.type = "text/javascript";
+    s.src = "https://cdn.jsdelivr.net/npm/js-cookie@3.0.1/dist/js.cookie.min.js"
+    document.head.appendChild(s).addEventListener("load", function () {
+        if (localStorage.getItem("cart") != null) {
+            var cart = JSON.parse(localStorage.getItem("cart"));
+            var cartCount = 0;
+            for (var product in cart) {
+                cartCount += cart[product];
+            }
+            document.getElementById('cart-count').innerHTML = cartCount;
+            const userEmail = Cookies.get('userEmail');
+            const isLoggedIn = Cookies.get('isLoggedIn')
+    
+            if (isLoggedIn) {
+                // El usuario está autenticado, cambia el contenido del contenedor de acciones de usuario
+                const userActionsContainer = document.querySelector('.user-actions');
+    
+                // Iniciar sesión (oculto)
+                const loginLink = document.getElementById('loginLink');
+                loginLink.style.display = 'none';
+    
+                // Cerrar sesión (visible)
+                const dropdown = userActionsContainer.querySelector('.dropdown');
+                const userEmailSpan = document.getElementById('userEmail');
+                userEmailSpan.innerText = userEmail;  // Actualiza el contenido con el correo electrónico
+                dropdown.style.display = 'inline';
+    
+                logoutLink.addEventListener('click', function () {
+                    // Elimina las cookies al cerrar sesión
+                    Cookies.remove('userEmail');
+                    Cookies.remove('isLoggedIn');
+                });
+            }
+    
+            // Inicializa Bootstrap para el menú desplegable
+            new bootstrap.Dropdown(document.getElementById('userDropdown'));
         }
-        document.getElementById('cart-count').innerHTML = cartCount;
-        const userEmail = Cookies.get('userEmail');
-        const isLoggedIn = Cookies.get('isLoggedIn')
+    });
 
-        if (isLoggedIn) {
-            // El usuario está autenticado, cambia el contenido del contenedor de acciones de usuario
-            const userActionsContainer = document.querySelector('.user-actions');
-
-            // Iniciar sesión (oculto)
-            const loginLink = document.getElementById('loginLink');
-            loginLink.style.display = 'none';
-
-            // Cerrar sesión (visible)
-            const dropdown = userActionsContainer.querySelector('.dropdown');
-            const userEmailSpan = document.getElementById('userEmail');
-            userEmailSpan.innerText = userEmail;  // Actualiza el contenido con el correo electrónico
-            dropdown.style.display = 'inline';
-
-            logoutLink.addEventListener('click', function () {
-                // Elimina las cookies al cerrar sesión
-                Cookies.remove('userEmail');
-                Cookies.remove('isLoggedIn');
-            });
-        }
-
-        // Inicializa Bootstrap para el menú desplegable
-        new bootstrap.Dropdown(document.getElementById('userDropdown'));
-    }
 });
 fetch('/footer.html').then(function (response) {
     return response.text();
 }).then(function (data) {
     var footer = document.getElementById('footer')
+    if(footer == null){
+        return;
+    }
     footer.innerHTML = data;
     footer.classList.add('mt-auto');
 });
