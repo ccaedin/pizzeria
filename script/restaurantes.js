1a455b0d5bbcd84dd0b253e8c72a26f4992b0095
alfonsosf-dev <100472286@alumnos.uc3m.es>
2023-12-13 13:19:45 +0100

    Updated restaurantes

--- script/restaurantes.js
+++ script/restaurantes.js
@@ -1,10 +1,11 @@
 var map;
 var centerll = { lat: 40.43298728841444, lng: -3.689733673350224 };
+var selectedMarker = null;
 
 // Datos ficticios de restaurantes
 var restaurantes = [
-    { id: 1, name: "Restaurante 1", address: "Dirección 1", phone: "123-456-789", lat: 40.43298728841444, lng: -3.689733673350224 },
-    { id: 2, name: "Restaurante 2", address: "Dirección 2", phone: "987-654-321", lat: 41.380445634454375, lng: 2.1862795923315996 }
+    { id: 1, lat: 40.43298728841444, lng: -3.689733673350224},
+    { id: 2, lat: 41.380445634454375, lng: 2.1862795923315996}
 ];
 
 function initMap() {
@@ -17,22 +18,66 @@ function initMap() {
     restaurantes.forEach(function (restaurante) {
         var marker = new google.maps.Marker({
             position: { lat: restaurante.lat, lng: restaurante.lng },
-            title: restaurante.name,
             map: map
         });
 
         // Agrega un evento al hacer clic en un marcador
         marker.addListener('click', function () {
-            expandRestaurantInfo(restaurante.name, restaurante.address, restaurante.phone);
+            // Restaurar color del marcador previamente seleccionado
+            if (selectedMarker !== null) {
+                selectedMarker.setIcon('https://maps.google.com/mapfiles/ms/micons/red-dot.png');
+                cambiarColorDiv(selectedMarker.get('id'), 'white');
+                ocultarBoton(selectedMarker.get('id'));
+            }
+
+            // Cambiar color del marcador seleccionado
+            marker.setIcon('https://maps.google.com/mapfiles/ms/micons/blue-dot.png');
+            cambiarColorDiv(marker.get('id'), 'lightblue');
+            mostrarBoton(marker.get('id'));
+
+            // Almacena el marcador seleccionado
+            selectedMarker = marker;
         });
+
+        // Asigna un identificador único al marcador
+        marker.set('id', restaurante.id);
     });
 }
 
-// Función para expandir la información del restaurante
-function expandRestaurantInfo(name, address, phone) {
-    var infoContainer = $("#restaurant-info");
-    infoContainer.html(`<h3>${name}</h3><p>Dirección: ${address}</p><p>Teléfono: ${phone}</p>`);
+// Función para cambiar el color del div correspondiente al restaurante del marcador
+function cambiarColorDiv(restauranteId, color) {
+    var div = document.querySelector('[data-id="' + restauranteId + '"]');
+    if (div) {
+        // Remover la clase 'bg-primary' de todos los elementos
+        document.querySelectorAll('.restaurante-item').forEach(function (element) {
+            element.classList.remove('bg-primary', 'text-white');
+        });
 
-    // Realizar la animación de expansión
-    infoContainer.slideDown();
+        // Aplicar las clases de Bootstrap al elemento correspondiente
+        div.classList.add('bg-primary', 'text-white');
+    }
 }
+
+// Función para mostrar el botón correspondiente al restaurante del marcador
+function mostrarBoton(restauranteId) {
+    var boton = document.querySelector('[data-id="' + restauranteId + '"] .btn-recoger');
+    if (boton) {
+        boton.style.display = 'inline-block';
+        boton.addEventListener('click', function () {
+            // Redirige a index.html
+            window.location.href = 'index.html';
+        });
+    }
+}
+
+// Función para ocultar el botón correspondiente al restaurante del marcador
+function ocultarBoton(restauranteId) {
+    var boton = document.querySelector('[data-id="' + restauranteId + '"] .btn-recoger');
+    if (boton) {
+        boton.style.display = 'none';
+        boton.removeEventListener('click', null);
+    }
+}
+
+
+
